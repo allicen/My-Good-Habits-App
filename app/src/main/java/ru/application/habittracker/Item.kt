84edcb8bef24c0756eea88bbe82136f864965241
Elena Rubleva <ellena.hella@yandex.ru>
2020-04-12 00:53:50 +0500

    Добавление FAB.

--- app/src/main/java/ru/application/habittracker/Item.kt
+++ app/src/main/java/ru/application/habittracker/Item.kt
@@ -2,10 +2,16 @@ package ru.application.habittracker
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.item.*
 
 class Item: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.item)
+    }
+
+    override fun onStart() {
+        super.onStart()
     }
 }
--- app/src/main/java/ru/application/habittracker/List.kt
+++ app/src/main/java/ru/application/habittracker/List.kt
@@ -1,13 +1,21 @@
 package ru.application.habittracker
 
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 
-class List: AppCompatActivity(){
+import kotlinx.android.synthetic.main.main.*
+
+class List : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
     }
-
-
 }
