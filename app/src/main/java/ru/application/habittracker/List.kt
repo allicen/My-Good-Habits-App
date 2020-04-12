933c5da629d76070b2092895da30c58bbd1371ef
Elena Rubleva <ellena.hella@yandex.ru>
2020-04-13 00:04:46 +0500

    Разработка макета. Добапвлены шаблоны, логотипы, цветовая гамма, содержимое пустых страниц, форма для добавления привычки.

--- app/src/main/java/ru/application/habittracker/List.kt
+++ app/src/main/java/ru/application/habittracker/List.kt
@@ -1,21 +1,36 @@
 package ru.application.habittracker
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.list.*
 
 import kotlinx.android.synthetic.main.main.*
 
 class List : AppCompatActivity() {
 
+    var habitCount = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
-        setSupportActionBar(toolbar)
+//        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        if (habitCount == 0) {
+            empty_list.visibility = TextView.VISIBLE
+        }
+
+        fab.setOnClickListener {
+//                view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+
+            Log.e("tag", "Открыто окно создания привычки")
+            val addHabit = Intent(this, Item::class.java)
+            startActivity(addHabit)
         }
     }
 }
